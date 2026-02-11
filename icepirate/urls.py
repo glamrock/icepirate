90429a95bdb200018fc4668fc1fb1877a022cb9f
Griffin Boyce <griffin@cryptolab.net>
2026-02-11 11:30:00 +0000

    Update URL configuration for django-allauth
    Replace django-registration-redux URL patterns with django-allauth:
    - Remove custom logout view that duplicated allauth functionality
    - Replace registration.backends.default.urls with allauth.urls
    - Keep login redirect for backward compatibility
    
    This simplifies the URL configuration while maintaining the same
    user-facing authentication endpoints.

--- icepirate/urls.py
+++ icepirate/urls.py
@@ -1,6 +1,5 @@
 from django.urls import include, path, re_path
 from django.views.generic.base import RedirectView
-from django.contrib.auth.views import LogoutView
 
 from message.views import short_url_redirect
 from core import views as core_views
@@ -10,8 +9,7 @@ from django.contrib import admin
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('accounts/logout/', LogoutView.as_view(), {'next_page': '/accounts/login'}),
-    path('accounts/', include('registration.backends.default.urls')),
+    path('accounts/', include('allauth.urls')),
     path('login/', RedirectView.as_view(url='/accounts/login/')),
     path('user-management/', core_views.user_management, name='user_management'),
 
